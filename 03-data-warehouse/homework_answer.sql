f314e4bfa826cc374c57c353103c5f27b6ce016b
Adham <adhamsiwi.as@gmail.com>
2024-05-09 12:34:36 +0700

    add homework answer

---
+++ 03-data-warehouse/homework_answer.sql
@@ -0,0 +1,39 @@
+-- Question 1: Create External Table
+
+create or replace external table taxi_data.external_table_green_taxi_data
+options (
+  format = 'PARQUET',
+  URIS=['gs://green_taxi_homework_module_3/*']
+)
+
+-- Question 2:
+create or replace table taxi_data.green_taxi_data 
+as (
+  select *
+  from `imposing-vista-416522.taxi_data.external_table_green_taxi_data`
+)
+
+create or replace table `taxi_data.green_taxi_partitioned_table`
+partition by date(lpep_pickup_datetime)
+cluster by pulocationid as (
+  select * from `imposing-vista-416522.taxi_data.green_taxi_data`
+)
+
+-- Question 4: Partition and Cluster
+create or replace table `taxi_data.green_taxi_partitioned_table`
+partition by date(lpep_pickup_datetime)
+cluster by pulocationid as (
+  select * from `imposing-vista-416522.taxi_data.green_taxi_data`
+)
+
+-- Question 5 - Performance Compare Partitioned vs Materialized Table
+-- 12.82 MB
+select distinct(PULocationID)
+from `imposing-vista-416522.taxi_data.green_taxi_data`
+where lpep_pickup_datetime between '2022-06-01' and '2022-06-30';
+
+-- 1.12 MB
+select distinct(PULocationID)
+from `imposing-vista-416522.taxi_data.green_taxi_partitioned_table`
+where lpep_pickup_datetime between '2022-06-01' and '2022-06-30';
+
