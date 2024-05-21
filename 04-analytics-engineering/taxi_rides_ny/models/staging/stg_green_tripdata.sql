0f491a4ae389343d5f41c37e9c0b46764b82478b
Adham Erlangga Siwi <adhamsiwi.as@gmail.com>
2024-05-21 23:59:28 +0000

    add macros 'payment_type_description'

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -24,7 +24,7 @@ renamed as (
         ehail_fee,
         airport_fee,
         total_amount,
-        payment_type,
+        {{ get_payment_type_description('payment_type') }} as payment_type_descripted,
         distance_between_service,
         time_between_service,
         trip_type,
